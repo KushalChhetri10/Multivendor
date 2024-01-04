946b00ba863490dfea79e6e80a5a84650977c362
KushalChhetri10 <kushalxetri10@gmail.com>
2024-01-04 18:07:57 +0545

    Added env file

--- backend/middleware/morgan.js
+++ backend/middleware/morgan.js
@@ -2,14 +2,14 @@ const morgan = require("morgan");
 const logger = require("./logger");
 
 const successResponseFormat = `:method :url :status - :response-time ms`;
-const errorResponseFormat = `:method :url :status - :response-time ms - message: :message`;
+const errorResponseFormat = `:method :url :status - :response-time ms`;
 
-const successHandler = morgan("combined", {
+const successHandler = morgan(successResponseFormat, {
   skip: (req, res) => res.statusCode >= 400,
   stream: { write: (message) => logger.info(message.trim()) },
 });
 
-const errorHandler = morgan("combined", {
+const errorHandler = morgan(errorResponseFormat, {
   skip: (req, res) => res.statusCode < 400,
   stream: { write: (message) => logger.error(message.trim()) },
 });
--- frontend/src/config/axiosConfig.ts
+++ frontend/src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const lwpAxios: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   timeout: 5000,
 });
 
